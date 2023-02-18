57cda26ed363b49ed3dec0eeca8a53186b7eb8eb
Andrew <56186268+Andrew-Marty00shenko@users.noreply.github.com>
2023-02-18 23:02:47 +0300

    Create login

--- client/src/api/userApi.ts
+++ client/src/api/userApi.ts
@@ -1,10 +1,16 @@
 import axios from "./axios";
 
 import type { IUserRegisterForm } from "pages/Register";
+import type { IUserLoginForm } from "pages/Login";
 
 export default {
     register: async ({ name, email, password }: IUserRegisterForm) => {
         const response = await axios.post<IUserRegisterForm>('/user/registration', { name, email, password });
+
+        return response;
+    },
+    login: async ({ email, password }: IUserLoginForm) => {
+        const response = await axios.post<IUserLoginForm>('/user/login', { email, password });
 
         return response;
     }
--- client/src/routes/Router.tsx
+++ client/src/routes/Router.tsx
@@ -2,13 +2,13 @@ import type { FC } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Main from "pages/Main";
-// import Login from "pages/Login";
+import Login from "pages/Login";
 import Register from "pages/Register";
 
 const Router: FC = () => {
     return <Routes>
         <Route path="/" element={<Main />} />
-        {/* <Route path="/login" element={<Login />} /> */}
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
     </Routes>
 };
